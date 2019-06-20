bb762d2b281014d7de438c93b15786af6f40f123
Your Name <crystalgallivan@gmail.com>
2019-06-20 16:35:53 -0600

    navbar added - david

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,39 @@
 <template>
   <div id="app">
     <div class="container-fluid" id="nav">
-      <div class="row" id="navbar">
-        <div class="col"></div>
-        <div class="col"></div>
+      <div class="row">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <router-link to="/">Home |<span class="sr-only">(current)</span></router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown link
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
       <div class="row" id="main-body">
         <div class="col" id="sidebar"></div>
@@ -11,7 +41,7 @@
 
         </div>
       </div>
-      <router-link to="/">Home</router-link> |
+
       <router-link to="/">Costing</router-link> |
 
     </div>
