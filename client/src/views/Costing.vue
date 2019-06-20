3d4db8b0644b422af5abaf6dbdd655464fecb6f9
Dallin Fahnestock <dfahn03@gmail.com>
2019-06-20 16:20:36 -0600

    costing and recipe layout

---
+++ client/src/views/Costing.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="costing-tool">
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "",
+    props: [],
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
