95caf5bb02cf67ddb0045200f159364c2737a8bd
Dallin Fahnestock <dfahn03@gmail.com>
2019-06-21 17:36:46 -0600

    fixed spelling error on recipeIngredient

--- client/src/components/RecipeIngredient.vue
+++ client/src/components/RecipeIngredient.vue
@@ -2,15 +2,15 @@
   <tbody class="recipe-ingredient">
     <tr>
       <th scope="row">1</th>
-      <td><input type="text" placeholder="Distributor" v-model="actualIngrediant.distributor"></td>
-      <td><input type="text" placeholder="Product Number" v-model="actualIngrediant.productNumber"></td>
-      <td><input type="text" placeholder="Category" v-model="actualIngrediant.category"></td>
-      <td><input type="text" placeholder="Ingredient Name" v-model="actualIngrediant.itemName"></td>
-      <td><input type="text" placeholder="Brand" v-model="actualIngrediant.brand"></td>
-      <td><input type="text" placeholder="Package Size" v-model="actualIngrediant.packageSize"></td>
-      <td><input type="text" placeholder="Package Cost" v-model="actualIngrediant.packageCost"></td>
-      <td><input type="text" placeholder="Quantity" v-model="actualIngrediant.quantity"></td>
-      <td><input type="text" placeholder="Unit" v-model="actualIngrediant.unit"></td>
+      <td><input type="text" placeholder="Distributor" v-model="actualIngredient.distributor"></td>
+      <td><input type="text" placeholder="Product Number" v-model="actualIngredient.productNumber"></td>
+      <td><input type="text" placeholder="Category" v-model="actualIngredient.category"></td>
+      <td><input type="text" placeholder="Ingredient Name" v-model="actualIngredient.itemName"></td>
+      <td><input type="text" placeholder="Brand" v-model="actualIngredient.brand"></td>
+      <td><input type="text" placeholder="Package Size" v-model="actualIngredient.packageSize"></td>
+      <td><input type="text" placeholder="Package Cost" v-model="actualIngredient.packageCost"></td>
+      <td><input type="text" placeholder="Quantity" v-model="actualIngredient.quantity"></td>
+      <td><input type="text" placeholder="Unit" v-model="actualIngredient.unit"></td>
       <td v-if="ingredientCostCalc"><input type="text" placeholder="Ingredient Cost"
           v-model="itemCost">{{ingredientCostCalc()}}</td>
       <td v-else><input type="text" placeholder="Ingredient Cost" v-model="itemCost"></td>
@@ -35,7 +35,7 @@
         //TODO finish filling this out so it will auto populate ingredient cost
         //return size/cost * qquantity 
       },
-      actualIngrediant() { // returns appropriate ingredient object
+      actualIngredient() { // returns appropriate ingredient object
         return this.recipeIngredient ? this.recipeIngredient : this.ingredient
       }
     },
