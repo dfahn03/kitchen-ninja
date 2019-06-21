1b5f782b66141056e6f5b419c963af3372a73184
Your Name <crystalgallivan@gmail.com>
2019-06-21 13:30:08 -0600

    foodApi

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+
 
 Vue.use(Vuex)
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+let foodApi = Axios.create({
+  baseURL: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/",
+  // apiKey: '526b119c5amsh6ca0e9efb7065d9p14f001jsn5f03879042dd'
+})
+let auth = Axios.create({
+  baseURL: base + "auth/",
+  timeout: 3000,
+  withCredentials: true
+})
+
+let api = Axios.create({
+  baseURL: base + "api/",
+  timeout: 3000,
+  withCredentials: true
+})
 
 export default new Vuex.Store({
   state: {
