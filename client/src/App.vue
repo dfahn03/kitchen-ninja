a5ec9ae46d2520e1b748953a0fba6e4839ca93a1
Dallin Fahnestock <dfahn03@gmail.com>
2019-06-27 14:50:07 -0600

    user-select on app

--- client/src/App.vue
+++ client/src/App.vue
@@ -107,16 +107,13 @@
   }
 </script>
 <style>
-  app {
-    user-select: none;
-  }
-
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    user-select: none;
   }
 
   #home-nav {
