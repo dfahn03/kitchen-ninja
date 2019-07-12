a76f1c09576554141c592fa9230eaa752032d4d8
Your Name <crystalgallivan@gmail.com>
2019-07-12 15:43:30 -0600

    Fix

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import router from './router'
-import { STATES } from 'mongoose';
 
 Vue.use(Vuex)
 let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
