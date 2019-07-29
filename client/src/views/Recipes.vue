049c4bf61fb395e20e70a87bafcdef3c14cc80e7
Dallin Fahnestock <dfahn03@gmail.com>
2019-07-29 17:43:03 -0600

    recipes cleanup

--- client/src/views/Recipes.vue
+++ client/src/views/Recipes.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="container-fluid recipes">
+
     <div class="row recipes">
       <div class="col-12 text-center">
         <h1 class="recipes_title">Recipes</h1>
-        <button class="btn btn-sm create-recipe-bg mb-5 mt-3" @click="addRecipe()">Add Recipe</button>
+        <button class="btn btn-sm create-recipe-bg mb-5 mt-4" @click="addRecipe()">Add Recipe</button>
       </div>
     </div>
+
+    <!-- Recipe List -->
     <div class="row" id="recipe-by-station">
       <div v-for="menu in menus" class="col-6">
-        <!-- <li v-for="(group, station) in groups"> original, working but not sorted list -->
         <div v-for="(recipes, stationName) in menu">
           <h3 class="text-light station_name mt-4">{{stationName}}</h3>
-          <!-- <ul v-for="recipe in group" :key="recipe._id"> original, working but not sorted list -->
           <ul v-for="recipe in recipes" :key="recipe._id">
             <li class="recipe_list">{{recipe.name}} -- Cost per Portion: $
               {{(recipe.costPerRecipe / recipe.portions).toFixed(2)}}
               <img src="../assets/Plus-Icon-18.png" alt="Recipe Details" title="Recipe Details"
                 @click="itemClicked(recipe)" class="mb-1 ml-1 details-img">
-              <!-- <span><button class="btn btn-sm edit_recipe" @click="itemClicked(recipe)">+</button></!-->
             </li>
           </ul>
         </div>
       </div>
     </div>
+
     <!-- RECIPE MODAL -->
     <div id="recipe modal">
       <div id="my-modal" class="modal fade">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title">RECIPE DETAILS</h5>
+              <h5 class="modal-title">Recipe Details</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -47,24 +48,21 @@
             <div class="modal-footer justify-content-center">
               <img src="../assets/Edit-Icon-40.png" alt="Delete Recipe" title="Edit Recipe"
                 @click="sendRecipeToCosting()" class="edit-recipe">
-              <!-- <button type="button" class="btn btn-warning" @click="sendRecipeToCosting()">Edit Recipe</button> -->
               <img src="../assets/Trash-Can-Red-40.png" alt="Delete Recipe" title="Delete Recipe"
                 @click="deleteRecipe()" class="delete-recipe">
-              <!-- <button type="button" class="btn btn-danger" @click="deleteRecipe()">Delete Recipe</button> -->
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
   export default {
     name: "Recipes",
-    mounted() {
-      this.$store.dispatch("getRecipes")
-    },
+    mounted() { },
     data() {
       return {
         id: '',
@@ -103,15 +101,8 @@
           }
         });
         colMenus.push(menu)
-        // console.log(menu)
         return colMenus
-        // console.log(groups)
       },
-      // columnMenu() {
-      //   //menu object with keys of stations values of recipies
-      //   let colMenu = []
-      //   //array of menu objects, where the total recipies among the keys does not exceed 15
-      // }
     },
     methods: {
       addRecipe() {
@@ -136,7 +127,7 @@
         this.name = item.name;
         this.station = item.station;
         this.side = item.side;
-        this.costPerRecipe = item.costPerRecipe;
+        this.costPerRecipe = item.costPerRecipe.toFixed(2);
         this.portions = item.portions;
         this.portionSize = item.portionSize;
         this.portionUnit = item.portionUnit;
@@ -150,7 +141,6 @@
   .recipe_list {
     color: white;
     padding: 2px;
-    /* border: 1px solid whitesmoke; */
     font-size: 15px;
     font-family: 'Aleo', serif;
     font-size: 18px;
@@ -190,17 +180,8 @@
     font-size: 2em;
   }
 
-  /* TODO we may want to remove this when more recipes are rendering */
   #recipe-by-station {
     display: flex;
     justify-content: center;
   }
-
-  /* ul {
-    -ms-column-count: 2;
-    -o-column-count: 2;
-    -moz-column-count: 2;
-    -khtml-column-count: 2;
-    column-count: 2;
-  } */
 </style>
