cf766bb5200740b50ca0aa6cbd032e9a63e806d5
Dallin Fahnestock <dfahn03@gmail.com>
2019-08-09 18:47:05 -0600

    Active Recipe & Recipe Form

--- client/src/components/ActiveRecipe.vue
+++ client/src/components/ActiveRecipe.vue
@@ -5,11 +5,11 @@
         <tr class="row">
           <th class="col-sm-3">Recipe Name</th>
           <th class="col-sm-1">Portions</th>
-          <th class="col-sm-1">Size</th>
-          <th class="col-sm-1">Unit</th>
+          <th class="col-sm-2">Portion Size</th>
+          <!-- <th class="col-sm-1">Unit</th> -->
           <th class="col-sm-1">Side</th>
-          <th class="col-sm-1">Station</th>
-          <th class="col-sm-2">Calories</th>
+          <th class="col-sm-2">Station</th>
+          <th class="col-sm-1">Calories</th>
           <th class="col-sm-2">Allergens</th>
         </tr>
       </thead>
@@ -17,12 +17,12 @@
         <tr class="row">
           <td class="col-sm-3">{{activeRecipe.name}}</td>
           <td class="col-sm-1">{{activeRecipe.portions}}</td>
-          <td class="col-sm-1">{{activeRecipe.portionSize}}</td>
-          <td class="col-sm-1">{{activeRecipe.portionUnit}}</td>
+          <td class="col-sm-2">{{activeRecipe.portionSize}} {{activeRecipe.portionUnit}}</td>
+          <!-- <td class="col-sm-1">{{activeRecipe.portionUnit}}</td> -->
           <td class="col-sm-1">{{activeRecipe.side}}</td>
-          <td class="col-sm-1">{{activeRecipe.station}}</td>
-          <td class="col-sm-2">{{activeRecipe.calories}}</td>
-          <td class="col-sm-2">{{activeRecipe.allergens}}</td>
+          <td class="col-sm-2">{{activeRecipe.station}}</td>
+          <td class="col-sm-1">{{activeRecipe.calories}}</td>
+          <td class="col-sm-2" v-for="allergen in activeRecipe.allergens">{{allergen}},</td>
         </tr>
       </tbody>
     </table>
